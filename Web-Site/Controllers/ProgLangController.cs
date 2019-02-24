ee34e080e2a62b74f9f5698002aa243871a899c0
Stephen Fowles-White <fowleswhite@gmail.com>
2019-02-24 14:57:13 +0000

    Created Web Content

--- Web-Site/Controllers/ProgLangController.cs
+++ Web-Site/Controllers/ProgLangController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web_Site.Models;
 
 namespace Web_Site.Controllers
 {
@@ -11,7 +12,14 @@ namespace Web_Site.Controllers
         // GET: Need to review type of return https://www.tutorialsteacher.com/mvc/action-method-in-mvc
         public ActionResult ProgLang()
         {
-            return View();
+            var HTML = new ProgLang()
+            {
+                Languague = "HTML & CSS",
+                Frameworks = "HTML5, W3.CSS, BootStrap",
+                Description = "Expereince with using Customer Made Front End Webpages and ulitising framworks such as W3.CC and Bootstap. All to ensure"
+                               + "Your wesbite reaches the right people in the right way",
+            };
+            return View(HTML);
         }
     }
 }
--- Web-Site/Models/ProgLang.cs
+++ Web-Site/Models/ProgLang.cs
@@ -7,8 +7,8 @@ namespace Web_Site.Models
 {
     public class ProgLang
     {
-        public int Languague { get; set; }
+        public string Languague { get; set; }
         public string Frameworks { get; set; }
-        public int Description { get; set; }
+        public string Description { get; set; }
     }
 }
