f746ae853bf1bd7cd860e766c3a19f869088893b
Stephen Fowles-White <fowleswhite@gmail.com>
2019-02-25 08:31:44 +0000

    Setting up Array of Programming Lanaguages Data

--- Web-Site/Data/ProgLangData.cs
+++ Web-Site/Data/ProgLangData.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Web_Site.Models;
 
-namespace Web_Site
+namespace Web_Site.Data
 {
     public class ProgLangData
     {
+        private static ProgLang[] _progLang = new ProgLang[]
+        {
+            new ProgLang()
+
+                {
+                    Languague = "HTML & CSS",
+                    Frameworks = "HTML5, W3.CSS, BootStrap",
+                    Description = "Expereince with using Customer Made Front End Webpages and ulitising framworks such as W3.CC and Bootstap. All to ensure"
+                           + "Your wesbite reaches the right people in the right way",
+                },
+             new ProgLang()
+             {
+                    Languague = "HTML & CSS",
+                    Frameworks = "HTML5, W3.CSS, BootStrap",
+                    Description = "Expereince with using Customer Made Front End Webpages and ulitising framworks such as W3.CC and Bootstap. All to ensure"
+                           + "Your wesbite reaches the right people in the right way",
+              },
+              new ProgLang()
+              {
+                    Languague = "HTML & CSS",
+                    Frameworks = "HTML5, W3.CSS, BootStrap",
+                    Description = "Expereince with using Customer Made Front End Webpages and ulitising framworks such as W3.CC and Bootstap. All to ensure"
+                           + "Your wesbite reaches the right people in the right way",
+               },
+
+
+        };
     }
 }
--- Web-Site/Models/ProgLang.cs
+++ Web-Site/Models/ProgLang.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Web_Site.Models;
+using Web_Site.Data;
 
 namespace Web_Site.Models
 {
