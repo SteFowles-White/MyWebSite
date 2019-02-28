b7e03548308e623988f950333b8ae097588c6351
Stephen Fowles-White <fowleswhite@gmail.com>
2019-02-28 22:14:50 +0000

    Updates Contoller

--- Web-Site/Controllers/StephenFowlesWhiteController.cs
+++ Web-Site/Controllers/StephenFowlesWhiteController.cs
@@ -11,17 +11,24 @@ namespace Web_Site.Controllers
     public class StephenFowlesWhiteController : Controller
     {
         //create a private  StephenFowlesWhiteRepos field that hold null
+        private StephenFowles_White _stephenFowles_White = null;
 
         //create a construrtor that uses the private feild and 
         //assigns it a new set of data
+        public StephenFowlesWhiteController()
+            {
+            _stephenFowles_White = new StephenFowles_White();
+            }
 
-        
         // GET: StephenFowlesWhite
         public ActionResult Index()
         {
             //get the data using the get data method
+
+            var StephenFowlesWhiteView = _stephenFowles_White.GetStephensData();
+
             // retunr the assigned varable data to the view
-            return View();
+            return View(StephenFowlesWhiteView);
         }
     }
 }
