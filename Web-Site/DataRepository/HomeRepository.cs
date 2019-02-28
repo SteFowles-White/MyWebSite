ad1add321e0f2e089ccfc20760ea39fc045e6760
Stephen Fowles-White <fowleswhite@gmail.com>
2019-02-28 08:28:29 +0000

    - Setting up the Data and information for the home page - Created a Home Page Model - Set up HomeRespository

---
+++ Web-Site/DataRepository/HomeRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Web_Site.Models;
+
+namespace Web_Site.DataRepository
+{
+
+    //set up data that wil be used on the Home Page
+    public class HomeRepository
+    {
+        private static Home[] _homes = new Home[]
+        {
+            new Home()
+                {
+                    Title = "",
+                    TextHeading = ""
+                 },
+            new Home()
+                {
+                    Title = "",
+                    TextHeading = ""
+                 },
+            new Home()
+                {
+                    Title = "",
+                    TextHeading = ""
+                 }
+
+        };
+
+    }
+}
---
+++ Web-Site/Models/Home.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_Site.Models
+{
+    public class Home
+    {
+        public string Title { get; set; }
+
+        public string TextHeading { get; set; }
+
+        public string imgIcon
+        {
+            get
+            {
+                return Title.Replace(" ", "_") + ".jpg";
+            }
+        }
+
+    }
+}
