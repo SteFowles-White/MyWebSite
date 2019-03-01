adbc792ab98a31acac10870bdc06b44b171a2794
Stephen Fowles-White <fowleswhite@gmail.com>
2019-03-01 20:51:16 +0000

    - uploaded W3.css template - created a working Nav Bar - added Javascript functioanlity

---
+++ Web-Site/JavaScript/JavaScript.js
@@ -0,0 +1,12 @@
+﻿function OpenNavMenu() {
+    //cget the element
+    const openTab = document.getElementById("MenuTab");
+    //see if class w3-show is in the field
+    if (openTab.className.indexOf("w3-show") == -1) {
+        // if not add it to open the menu
+        openTab.className += " w3-show";
+    } else {
+        //if it is then remove it to close the window
+        openTab.className = openTab.className.replace(" w3-show", "");
+    }
+};
