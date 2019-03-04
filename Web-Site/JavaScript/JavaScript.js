5bae8b32542b4d31c3153d6c049651eb63cbc4bb
Stephen Fowles-White <fowleswhite@gmail.com>
2019-03-04 22:13:32 +0000

    set up Layout HTML

--- Web-Site/JavaScript/JavaScript.js
+++ Web-Site/JavaScript/JavaScript.js
@@ -1,5 +1,5 @@
 ﻿function OpenNavMenu() {
-    //cget the element
+    //get the element
     const openTab = document.getElementById("MenuTab");
     //see if class w3-show is in the field
     if (openTab.className.indexOf("w3-show") == -1) {
